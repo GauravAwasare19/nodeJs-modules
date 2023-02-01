99a9e4fdf95738b7d734567bd55f35ff98ce87b2
Gaurav <99120502+GauravAwasare19@users.noreply.github.com>
2023-02-01 22:54:55 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+
+//Creating a new file & it overWrites the data
+//fs.writeFileSync("read.txt","Hello world");
+//fs.writeFileSync("read.txt","Starting NodeJs Journey");
+
+
+//Creating a file and appending data & not overwriting
+//fs.appendFileSync("read.txt"," hello world");
+
+//Reading a file in binary form and then converting it to string
+//const buf_data= fs.readFileSync("read.txt");
+//const org_data = buf_data.toString();
+//console.log(org_data);
+
+//Reanaming a file
+//fs.renameSync("read.txt","readwrite.txt");
