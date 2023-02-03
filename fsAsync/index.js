b87193eef4ef3f4e33c4aa3c793b7c607dce72aa
Gaurav <99120502+GauravAwasare19@users.noreply.github.com>
2023-02-03 18:10:00 +0530

    Add files via upload

---
+++ fsAsync/index.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+
+/*
+fs.writeFile("read.txt","hello world",(err)=>{
+    console.log("file created");
+});
+*/
+
+/*
+fs.appendFile("read.txt"," This is Asynchronous appending",(err)=>{
+    console.log("data appended");
+});
+*/
+
+
+/*
+fs.readFile("read.txt","utf-8",(err,data)=>{
+    console.log(data);
+});
+*/
+
