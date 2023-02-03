11fda347c5041f4b95a48e4075a38e691eab6f22
Gaurav <99120502+GauravAwasare19@users.noreply.github.com>
2023-02-03 13:43:11 +0530

    Add files via upload

---
+++ challenge 1/index.js
@@ -0,0 +1,21 @@
+ const fs =require("fs")
+
+ //fs.mkdirSync("Gaurav");
+
+ //here Gaurav/bio.txt is written as the cmd is in prev folder
+ //fs.writeFileSync("Gaurav/bio.txt","Hello world");
+
+ //fs.appendFileSync("Gaurav/bio.txt","This is challenge 1");
+
+ //utf8 is used to convert the buffer data into string
+ //const data = fs.readFileSync("Gaurav/bio.txt","utf8");
+ //console.log(data);
+
+ 
+ //fs.renameSync("Gaurav/bio.txt","Gaurav/myBio.txt");
+
+ //this is used to delete file
+ //fs.unlinkSync("Gaurav/myBio.txt");
+
+ //This is used to delete the folder
+ //fs.rmdirSync("Gaurav");
