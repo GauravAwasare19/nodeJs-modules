0fb634f02eb92cfd14a5a2f56e696fd5062a7e3a
Gaurav <99120502+GauravAwasare19@users.noreply.github.com>
2023-02-03 19:11:54 +0530

    Add files via upload

---
+++ challenge 2/index1.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+
+/*
+fs.mkdir('challenge 2',(err)=>{
+    console.log("folder created");
+});
+*/
+
+/*
+fs.writeFile("./challenge 2/bio.txt","Hello World",(err)=>{
+    console.log("File created");
+});
+*/
+
+/*
+fs.appendFile("./challenge 2/bio.txt"," HELLO WORLD",(err)=>{
+    console.log("data appended");
+});
+*/
+
+/*
+fs.readFile("./challenge 2/bio.txt","utf-8",(err,data)=>{
+    console.log(data);
+});
+*/
+
+/*
+fs.rename("./challenge 2/bio.txt","./challenge 2/mybio.txt",(err)=>{
+    console.log("file renamed");
+});
+*/
+
+/*
+fs.unlink("./challenge 2/mybio.txt",(err)=>{
+    console.log("files deleted");
+});
+*/
+
+/*
+fs.rmdir("./challenge 2",(err)=>{
+    console.log("folder deleted");
+});
+*/
